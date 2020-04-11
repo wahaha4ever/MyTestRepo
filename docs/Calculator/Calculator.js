982333246c46ce17617091bd1d2f2e7077365073
wahaha4ever <55226440+wahaha4ever@users.noreply.github.com>
2020-04-11 14:46:57 +0800

    Calculator

--- docs/Calculator/Calculator.js
+++ docs/Calculator/Calculator.js
@@ -19,7 +19,7 @@ function UserButton(props) {
 }
 {/*<li><a class="page-link" href="#" data_a={props.a} data_b={props.b} data_oper={props.oper} key={props.keyx}>{props.keyx}</a></li>*/}
 function QuestionItem(props) {
-	const classNameBase = "btn btn-lg";
+	const classNameBase = "btn";
 	let className = props.selected ? "btn-primary" : "btn-outline-primary";
 	if (props.isans) {
 		if (props.iscorrect)
@@ -38,14 +38,26 @@ function QuestionItem(props) {
 
 function DisplayItem(props) {
 	let q = props.question;
-	let className = "";
-	if (q.ans && q.correct)
-		className = "correct";
-	else if (q.ans && !q.correct)
-		className = "error";
-	const symbol = q.oper == "0" ? "+" : "-";
+	let status = props.status;
+	let currentAns = props.currentAns;
+	
+	let className = status == GameStatus.INIT ? "hide" : "";
+	
+	//if (status == GameStatus.END) {
+	//	if (q.ans && q.correct)
+	//		className += " correct";
+	//	else if (q.ans && !q.correct)
+	//		className += " error";
+	//}
+	let symbol = ""
+	if (q.oper == "0") 
+		symbol = "+";
+	else if (q.oper == "1")
+		symbol = "-";
+	else
+		symbol = "X";
 	return(
-		<div class={className}>{q.a} {symbol} {q.b} = {q.ans}</div>
+		<div class={className}><h1>{q.a} {symbol} {q.b} = {currentAns}</h1></div>
 	)
 }
 
@@ -55,9 +67,75 @@ function checkAns(a, b, oper, ans){
 	if (oper == "0") {
 		return (a + b == ans);
 	}
-	else {
+	else if (oper == "1") {
 		return (a - b == ans);
 	}		
+	else {
+		return (a * b == ans);
+	}
+}
+
+function initQuestion2(config) {
+	let operator = [];
+	let questions = [];
+	if (config.plus)
+		operator.push(0);	//+
+	if (config.sub)
+		operator.push(1);	//-
+	if (config.multiply)
+		operator.push(2);	//*
+		
+	var fnMax = function(digit) {
+		let result = "0";
+		for (var i=0; i<digit; i++)
+			result += "9";
+		return parseInt(result, 10);	
+	}
+	
+	const plusMax = fnMax(config.plusDigit);
+	const subMax = fnMax(config.subDigit);
+	const multiplyMaxX = config.multiplyX;
+	const multiplyMaxY = config.multiplyY;
+	for(var i = 0; i < 10; i++){
+		let correct = false;
+		let oper = operator[getRandomInt(operator.length)];
+		let a = 0;
+		let b = 0;
+		let min = 1;
+		let max = 0;
+		if (oper == 0)
+			max = plusMax;
+		if (oper == 1)
+			max = subMax;
+		if (oper == 2)
+			max = multiplyMaxY;
+			
+		while (!correct) {			
+			b = getRandomInt(max) + min;
+			console.log(oper);
+			console.log(b);
+			for (var j = 0; j < 10; j++) {
+				a = getRandomInt(max) + min;
+				if (oper == 0) {
+					correct = true;
+					break;
+				}
+				if (oper == 1 && a >= b) {
+					correct = true;
+					break;
+				}
+				if (oper == 2) {
+					a = getRandomInt(multiplyMaxX) + min;
+					correct = true;
+					break;
+				}
+			}
+		}
+		questions.push({ a : a, b : b, oper : oper});
+	}
+		
+	return questions;	
+	
 }
 
 function initQuestion() {
@@ -118,10 +196,14 @@ class QuestionList extends React.Component {
 				<QuestionItem keyx={i+1} selected={selected} isans={isAns} iscorrect={isCorrect} onClick={()=> this.props.onClick(i)}/>
 			)
 		});
+		
+		const className = this.props.status == GameStatus.INIT ? "hide" : "";
 
 		return(
-			<div class="btn-group" role="group">
-				{list}
+			<div class={className}>
+				<div class="btn-group" role="group">
+					{list}
+				</div>
 			</div>
 		)
 	}
@@ -187,11 +269,96 @@ class Result extends React.Component {
 			className += " hide";
 		return(
 			<div class={className}>
-				<span>Game Completed!</span><br/>
-				<span>You got {cnt} out of {this.props.questions.length}</span><br/>
-				<button type="button" class="btn btn-primary" onClick={this.props.onClick}>Retry</button>
+				<div>
+					<span>Game Completed!</span><br/>
+					<span>You got {cnt} out of {this.props.questions.length}</span><br/>
+				</div>
+				<div class="btn-group">
+					<button type="button" class="btn btn-primary" value="reinit" onClick={this.props.onClickReinit}>ReInit</button>
+					<button type="button" class="btn btn-primary" value="retry" onClick={this.props.onClickRetry}>Retry</button>
+				</div>
 			</div>
 		)
+	}
+}
+
+class Setup extends React.Component {
+	constructor(props) {
+		super(props);		
+	}
+	
+
+	render() {
+		let className = this.props.status == GameStatus.INIT ? "" : "hide";
+		const { plus, plusDigit, sub, subDigit, multiply, multiplyX, multiplyY } =  this.props.config;
+		return(
+			<div class={className}>
+				<div class="container text-left">
+					<div class="card">
+						<div class="card-header bg-primary text-white">
+							<span>Setup</span>
+						</div>
+						<div class="card-body">
+							<div class="row">
+								<div class="col-12 offset-md-2 col-md-2">
+									<div class="form-check">
+										<input class="form-check-input" type="checkbox" id="plus" value="plus" checked={plus} onChange={this.props.onChangeChk} />
+										<label class="form-check-label" for="plus">Plus</label>
+									</div>
+								</div>
+								<div class="col-5 col-md-1"><input class="form-control" type="text" value={plusDigit} name="plusDigit" onChange={this.props.onChangeTxt} /></div>
+							</div>
+							<div class="row">
+								<div class="col-12 offset-md-2 col-md-2">
+									<div class="form-check">
+										<input class="form-check-input" type="checkbox" id="sub" value="sub" checked={sub} onChange={this.props.onChangeChk} />
+										<label class="form-check-label" for="sub">Minus</label>
+									</div>
+								</div>
+								<div class="col-5 col-md-1"><input class="form-control" type="text" value={subDigit} name="subDigit" onChange={this.props.onChangeTxt} /></div>
+							</div>
+							<div class="row">
+								<div class="col-12 offset-md-2 col-md-2">
+									<div class="form-check">
+										<input class="form-check-input" type="checkbox" id="multiply" value="multiply" checked={multiply} onChange={this.props.onChangeChk} />
+										<label class="form-check-label" for="multiply">Multiply</label>										
+									</div>
+								</div>
+								<div class="col-5 col-md-1"><input class="form-control" type="text" value={multiplyX} name="multiplyX" onChange={this.props.onChangeTxt} /></div>
+								<div class="col-1 col-md-1"><label class="form-check-label">X</label></div>
+								<div class="col-5 col-md-1"><input class="form-control" type="text" value={multiplyX} name="multiplyX" onChange={this.props.onChangeTxt} /></div>
+							</div>
+							<div class="row">
+								<div class="col-12 offset-md-2 col-md-2">
+									<button class="btn btn-primary btn-block" type="button" onClick={this.props.onClickStart}>Start</button>
+								</div>
+							</div>
+					{/*<div class="input-group">
+						<div class="input-group-prepend">
+							<input type="checkbox" value="plus" checked={plus} onChange={this.props.onChangeChk} />		
+							<label>Plus</label>
+						</div>
+						<input type="text" value={plusDigit} name="plusDigit" onChange={this.props.onChangeTxt} />
+					</div>	
+					<label>Plus</label>
+					<input type="checkbox" value="plus" checked={plus} onChange={this.props.onChangeChk} />
+					<input type="text" value={plusDigit} name="plusDigit" onChange={this.props.onChangeTxt} />
+					<br/>
+					<label>Minus</label>
+					<input type="checkbox" value="sub" checked={sub} onChange={this.props.onChangeChk} />
+					<input type="text" value={subDigit} name="subDigit" onChange={this.props.onChangeTxt} />
+					<br/>
+					<label>Multiply</label>
+					<input type="checkbox" value="multiply" checked={multiply} onChange={this.props.onChangeChk} />
+					<input type="text" value={multiplyX} name="multiplyX" onChange={this.props.onChangeTxt} /> x 
+					<input type="text" value={multiplyY} name="multiplyY" onChange={this.props.onChangeTxt} />
+					<br/>
+					<button type="button" onClick={this.props.onClickStart}>Start</button>*/}
+						</div>
+					</div>
+				</div>
+			</div>
+		);
 	}
 }
 
@@ -200,12 +367,31 @@ class Game extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			status : GameStatus.PROCESS, 
+			config : {
+				plus : true,
+				plusDigit : 2,
+				sub : true,
+				subDigit : 2,
+				multiply : true,
+				multiplyX : 10,
+				multiplyY : 10
+			},
+			status : GameStatus.INIT, 
 			gameType : 1,			// "1" : one way;
 			questions : initQuestion(),
 			currentIdx : 0,
 			currentAns : null
 		}
+	}
+	
+	fnInit(config) {
+		let questions = initQuestion2(config);
+		this.setState({
+			questions : questions,
+			status : GameStatus.PROCESS,
+			currentIdx : 0,
+			currentAns : null
+		});
 	}
 	
 	fnQestionChange(i) {
@@ -225,13 +411,46 @@ class Game extends React.Component {
 		});
 	}
 	
-	handleRetryClick() {
+	handleInit = e =>  {
+		this.fnInit(this.state.config);
+	}
+	
+	handleTxtChanged = e => {
+		const key = e.target.name;
+		let value = e.target.value;
+		if (value < 1)
+			value = 2;
+		console.log(value);
 		this.setState({
-			status : GameStatus.PROCESS,
-			questions : initQuestion(),
-			currentIdx : 0,
-			currentAns : null
-		})	
+			config :{
+				...this.state.config,
+				[key] : value
+			}
+		});
+	}
+	
+	handleTickChanged = e => {
+		const key = e.target.value;
+		console.log(e.target);
+		this.setState({
+			config :{
+				...this.state.config,
+				[key] : !this.state.config[key]
+			}
+		});
+		console.log(this.state.config[key]);
+	}
+	
+	handleResultClick = e => {
+		const value = e.target.value;
+		if (value == "retry") {
+			this.fnInit(this.state.config);
+		}
+		else {
+			this.setState({
+				status : GameStatus.INIT
+			});
+		}
 	}	
 	
 	//change question by user
@@ -284,11 +503,12 @@ class Game extends React.Component {
 		const symbol = currentQ.oper == "0" ? "+" : "-";
 		return(
 			<div>
+				<Setup status={this.state.status} config={this.state.config} onChangeChk={this.handleTickChanged} onChangeTxt={this.handleTxtChanged} onClickStart={this.handleInit} />
 				<QuestionList questions={this.state.questions} currentIdx={this.state.currentIdx} status={this.state.status} onClick={(i) => this.handleQuestionClick(i)}/>	
-				<div class="display"><h1>{currentQ.a} {symbol} {currentQ.b} = {this.state.currentAns}</h1></div>				
-				{/*<DisplayItem question={currentQ}/>*/}
+				{/*<div class="display"><h1>{currentQ.a} {symbol} {currentQ.b} = {this.state.currentAns}</h1></div>*/}
+				<DisplayItem question={currentQ} status={this.state.status} currentAns={this.state.currentAns}/>
 				<UserInput questions={this.state.questions} status={this.state.status} onClick={(i) => this.handleClick(i)}/>
-				<Result questions={this.state.questions} status={this.state.status} onClick={() => this.handleRetryClick()}/>
+				<Result questions={this.state.questions} status={this.state.status} onClickRetry={this.handleResultClick} onClickReinit={this.handleResultClick}/>
 			</div>
 		);
 	}
